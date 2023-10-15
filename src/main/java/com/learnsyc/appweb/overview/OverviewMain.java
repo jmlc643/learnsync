fbd93988e804c9d2556c16fff66ffa3c3bc93763
jmlc643 <josemarialuyocampos@gmail.com>
2023-10-14 23:16:19 -0500

    Segundo video Ali

---
+++ src/main/java/com/learnsyc/appweb/overview/OverviewMain.java
@@ -0,0 +1,76 @@
+package com.learnsyc.appweb.overview;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Optional;
+
+public class OverviewMain {
+    static String AuthenticatedUsername = "anónimo";
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static ArrayList<Usuario> array = new ArrayList<>();
+    public static void main(String[] args) throws IOException{
+        
+        String opc = "";
+        do{
+            System.out.println("Menú");
+            System.out.println("---------------------------------");
+            System.out.println("Bienvenido "+ AuthenticatedUsername);
+            System.out.println("---------------------------------");
+            System.out.println("A. Crear usuario");
+            System.out.println("B. Listar usuario");
+            System.out.println("C. Login");
+            System.out.println("D. Cerrar sesión");
+            System.out.println("Z. Salir");
+            System.out.println("Seleccione una opción: ");
+            opc = br.readLine();
+
+            switch(opc.toUpperCase()){
+                case "A" -> crearUsuario();    
+                case "B" -> listarUsuario();                
+                case "C" -> autenticarUsuario(); 
+                case "D" -> cerrarSesion();
+                default -> System.out.println("Opción incorrecta");
+            }
+        }while(!opc.toUpperCase().equals("Z"));
+    }
+    
+    private static void cerrarSesion() {
+        AuthenticatedUsername = "anónimo";
+    }
+
+    private static void autenticarUsuario() throws IOException {
+        System.out.println("Ingrese su usuario");
+        String user = br.readLine();
+        System.out.println("Ingrese su password");
+        String password = br.readLine();
+
+        Optional<Usuario> result = array.stream().filter(usuario -> {
+            return usuario.getUser().equalsIgnoreCase(user) 
+            && usuario.getPassword().equalsIgnoreCase(password);   
+        }).findFirst();
+        if (result.isPresent()){
+            System.out.println("Bienvenido " + result.get().getNombre());
+            AuthenticatedUsername = result.get().getUser();
+        } else {
+            System.out.println("Usuario o contraseña incorrecto");
+        }
+    }
+    private static void listarUsuario() {
+        array.stream().map(usuario -> usuario.getNombre().toUpperCase()).forEach(nombre ->{
+            System.out.println("-" + nombre);
+        });
+    }
+
+    private static void crearUsuario() throws IOException {
+        System.out.println("Ingrese su nombre");
+        String nombre = br.readLine();
+        System.out.println("Ingrese su usuario");
+        String user = br.readLine();
+        System.out.println("Ingrese su password");
+        String password = br.readLine();
+        Usuario usuario = new Usuario(nombre, user, password);
+        array.add(usuario);
+    }
+}
---
+++ src/main/java/com/learnsyc/appweb/overview/Usuario.java
@@ -0,0 +1,45 @@
+package com.learnsyc.appweb.overview;
+
+public class Usuario {
+    String nombre;
+    String user;
+    String password;
+
+    public Usuario(){
+    }
+
+    public Usuario(String nombre, String user, String password){
+        this.nombre = nombre;
+        this.user = user;
+        this.password = password;
+    }
+
+    @Override
+    public String toString(){
+        return "Usuario(nombre: " + nombre + "\tuser: " + user +"\tpassword: " + password + " )"; 
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public String getUser(){
+        return user;
+    }
+
+    public void setUser(String user){
+        this.user = user;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPassowrd(String password){
+        this.password = password;
+    }
+}
