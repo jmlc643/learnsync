b361e6c3096d5061a385a6821208f6db01b321b4
jmlc643 <josemarialuyocampos@gmail.com>
2023-10-29 22:55:23 -0500

    Conectando postman con el front

--- src/main/java/com/learnsyc/appweb/controllers/UserController.java
+++ src/main/java/com/learnsyc/appweb/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.learnsyc.appweb.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,7 @@ import com.learnsyc.appweb.services.UserService;
 
 @RestController
 @RequestMapping("user")
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
     @Autowired UserService userService; 
